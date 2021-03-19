0915e0add103c4006d168e5ebf445788545a9e3c
Kenny <kenny.rakotoarimanana@gmail.com>
2021-03-19 22:26:30 +0300

    Change environment apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8010/api'
+  apiUrl: 'https://backmbdsmiantsazo2021.herokuapp.com/api'
+  // apiUrl: 'http://localhost:8010/api'
 };
 
 /*
